90b7157cd853440a0bde7f46715c7a52a76d69b2
Marcel Koch <marcel.koch@kit.edu>
2026-03-31 16:26:43 +0200

    change ScaledIdentityAddable to multivector

--- core/base/lin_op.cpp
+++ core/base/lin_op.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include <ginkgo/core/base/lin_op.hpp>
+#include <ginkgo/core/base/multivector.hpp>
 
 namespace gko {
 
@@ -11,6 +12,17 @@ bool LinOp::supports_mixed_precision() const noexcept { return false; }
 
 
 precision LinOp::get_precision() const noexcept { return value_t_; }
+
+
+void ScaledIdentityAddable::add_scaled_identity(ptr_param<const MultiVector> a,
+                                                ptr_param<const MultiVector> b)
+{
+    GKO_ASSERT_IS_SCALAR(a);
+    GKO_ASSERT_IS_SCALAR(b);
+    auto ae = make_temporary_clone(as<LinOp>(this)->get_executor(), a);
+    auto be = make_temporary_clone(as<LinOp>(this)->get_executor(), b);
+    add_scaled_identity_impl(ae.get(), be.get());
+}
 
 
 }  // namespace gko
--- include/ginkgo/core/matrix/csr.hpp
+++ include/ginkgo/core/matrix/csr.hpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2017 - 2025 The Ginkgo authors
+// SPDX-FileCopyrightText: 2017 - 2026 The Ginkgo authors
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
@@ -1677,7 +1677,8 @@ private:
     array<index_type> row_ptrs_;
     array<index_type> srow_;
 
-    void add_scaled_identity_impl(const LinOp* a, const LinOp* b) override;
+    void add_scaled_identity_impl(const MultiVector* a,
+                                  const MultiVector* b) override;
 };
 
 
