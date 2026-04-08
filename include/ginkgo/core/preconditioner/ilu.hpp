44b129226df7803c6d44170770b18ef41bf9ba74
Marcel Koch <marcel.koch@kit.edu>
2026-04-08 12:27:57 +0200

    fixup! fix hierarchy change for preconditioner

--- include/ginkgo/core/preconditioner/ilu.hpp
+++ include/ginkgo/core/preconditioner/ilu.hpp
@@ -14,7 +14,6 @@
 #include <ginkgo/core/base/exception.hpp>
 #include <ginkgo/core/base/exception_helpers.hpp>
 #include <ginkgo/core/base/lin_op.hpp>
-#include <ginkgo/core/base/precision_dispatch.hpp>
 #include <ginkgo/core/base/type_traits.hpp>
 #include <ginkgo/core/config/config.hpp>
 #include <ginkgo/core/config/registry.hpp>
